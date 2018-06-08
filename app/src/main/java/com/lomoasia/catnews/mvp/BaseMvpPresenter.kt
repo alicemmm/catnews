6ecc76f000e3c9833a85516333890ea57dcf99dc
guoyazhou <guoyazhou@xiaomi.com>
2018-06-08 14:15:04 +0800

    aa

--- app/src/main/java/com/lomoasia/catnews/mvp/BaseMvpPresenter.kt
+++
@@ -1,7 +0,0 @@
-package com.lomoasia.catnews.mvp
-
-interface BaseMvpPresenter<in V : BaseMvpView> {
-    fun attachView(view: V)
-
-    fun detachView()
-}
--- app/src/main/java/com/lomoasia/catnews/view/MainActivity.kt
+++ app/src/main/java/com/lomoasia/catnews/view/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.lomoasia.catnews.view
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.lomoasia.catnews.R
+import com.lomoasia.catnews.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
